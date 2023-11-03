6a36f3c4501508c6427a2c631ca06277515f54e0
KishMiracle <miraclekish@gmail.com>
2023-11-03 06:54:41 +0100

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    pattern = r'{}(?!{})'.format(separator, separator.join(map(re.escape, fields)))
+    return re.sub(pattern, redaction, message)
