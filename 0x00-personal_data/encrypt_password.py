d547ecb289cc5933e6ac51941c03d8074ec7ccbe
KishMiracle <miraclekish@gmail.com>
2023-11-05 04:36:56 +0100

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,8 +5,8 @@ import bcrypt
 def hash_password(password):
     # Generate a random salt
     salt = bcrypt.gensalt()
-    
+
     # Hash the password with the generated salt
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    
+
     return hashed_password
