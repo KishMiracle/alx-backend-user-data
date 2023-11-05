4877f505ec23e82ecdbb9b1c3d4b3384a062751f
KishMiracle <miraclekish@gmail.com>
2023-11-05 04:08:21 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,14 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
-    pattern = """r'{}(?!{})'.format(separator,
-            separator.join(map(re.escape, fields)))"""
-    return re.sub(pattern, redaction, message)
+class LogFilter:
+    def __init__(self, fields):
+        self.fields = fields
+
+    def filter_datum(self, redaction, message, separator):
+        pattern = r'({})(?={}|$)'.format('|'.join(map(re.escape, self.fields)), re.escape(separator))
+        return re.sub(pattern, redaction, message)
+
+    def format(self, message, separator):
+        redaction = 'REDACTED'
+        return self.filter_datum(redaction, message, separator)
